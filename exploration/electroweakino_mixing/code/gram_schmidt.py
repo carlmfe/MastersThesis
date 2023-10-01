e46907d6a8c53e10cc1f345eabbd1e487a68f166
carlmfe <cmfevang@gmail.com>
2023-10-01 19:14:05 +0200

    Document code, nearly complete.

--- exploration/electroweakino_mixing/code/gram_schmidt.py
+++ exploration/electroweakino_mixing/code/gram_schmidt.py
@@ -1,29 +1,44 @@
 import numpy as np
+from numpy.typing import NDArray
 from sys import float_info
 EPSILON = float_info.epsilon
+TOL = np.sqrt(EPSILON)
 
 
-def inner_product(v: np.ndarray, u: np.ndarray) -> float | complex:
-    """Compute the inner product v^dagger * u for vector-like ndarrays v and u."""
+def inner_product(
+    v: NDArray[np.complex64],
+    u: NDArray[np.complex64]
+) -> float | complex:
+    """Compute the inner product v^dagger * u for vector-like ndarrays v and
+    u."""
     return np.squeeze(v.conj().T @ u)
 
 
-def magnitude(v: np.ndarray) -> float:
-    """Compute the magnitude |v| = sqrt(v^dagger * v) for a vector-like ndarray v."""
+def magnitude(v: NDArray[np.complex64]) -> float:
+    """Compute the magnitude |v| = sqrt(v^dagger * v) for a vector-like
+    ndarray v."""
     return np.sqrt(inner_product(v, v))
 
 
-def normalize(v: np.ndarray) -> np.ndarray:
+def normalize(v: NDArray[np.complex64]) -> NDArray[np.complex64]:
     """Normalize a vector-like ndarray v, returning v/|v|."""
     return v / magnitude(v)
 
 
-def projection(u: np.ndarray, v: np.ndarray) -> np.ndarray:
-    """Compute the projection of v onto u for two vector-like ndarrays u and v."""
+def projection(
+    u: NDArray[np.complex64],
+    v: NDArray[np.complex64]
+) -> NDArray[np.complex64]:
+    """Compute the projection of v onto u for two vector-like ndarrays u and
+    v."""
     return inner_product(u, v) / inner_product(u, u) * u
 
 
-def shift(arr: np.ndarray, old_idx: int, new_idx: int, axis=0) -> np.ndarray:
+def shift(
+    arr: NDArray[np.complex64],
+    old_idx: int, new_idx: int,
+    axis=0
+) -> NDArray[np.complex64]:
     """Shift an element in arr from old_idx to new_idx along given axis."""
     result = arr.copy()
     other_values = np.delete(result, old_idx, axis=axis)
@@ -31,13 +46,40 @@ def shift(arr: np.ndarray, old_idx: int, new_idx: int, axis=0) -> np.ndarray:
     return result
 
 
-def complete_nxn_basis(v: np.ndarray, n: int, index=0) -> np.ndarray:
-    """Given a vector-like ndarray v, return a matrix-like ndarray V whose columns form a basis
-    for the vector space C^n or R^n, depending on the dtype of v.
+def isLinearlyDependent(
+    u: NDArray[np.complex64],
+    v: NDArray[np.complex64],
+    get_prop=False
+):
+    nonzeros = np.nonzero(v)
+    proportionality = u[nonzeros] / v[nonzeros]
+    # If the components of u are the same multiple of the components of v,
+    # the std(u/v) should be small. Furthermore, the components of u
+    # corresponding to the zero-components of v should be zero.
+    if abs(np.std(proportionality)) < TOL and all(np.delete(u, nonzeros)) < TOL:
+        if get_prop:
+            return True, np.mean(proportionality)
+        else:
+            return True
+    else:
+        if get_prop:
+            return False, None
+        else:
+            return False
+
+
+def complete_nxn_basis(
+    v: NDArray[np.complex64],
+    n: int,
+    index=0
+) -> NDArray[np.complex64]:
+    """Given a vector-like ndarray v, return a matrix-like ndarray V whose
+    columns form a basis for the vector space C^n or R^n, depending on
+    the dtype of v.
     The vector v is embedded in V such that V[:, index] = v.
 
     Args:
-        v (np.ndarray): Vector-like ndarray to be used in the basis.
+        v (NDArray[np.complex64]): Vector-like ndarray to be used in the basis.
         n (int): Dimensionality of vector space.
         index (int, optional): Index of V to place v. Defaults to 0.
 
@@ -46,7 +88,7 @@ def complete_nxn_basis(v: np.ndarray, n: int, index=0) -> np.ndarray:
         i.e. the zero-vector.
 
     Returns:
-        np.ndarray: V, an ndarray with shape (n, n) that constitues a basis for
+        NDArray[np.complex64]: V, an ndarray with shape (n, n) that constitues a basis for
                     an n-dimensional vector space.
     """
     eye = np.eye(n, dtype=v.dtype)  # The trivial basis for the vector space.
@@ -61,18 +103,18 @@ def complete_nxn_basis(v: np.ndarray, n: int, index=0) -> np.ndarray:
     raise RuntimeError(f"Could not complete basis for v={v}")
 
 
-def gram_schmidt(V: np.ndarray) -> np.ndarray:
+def gram_schmidt(V: NDArray[np.complex64]) -> NDArray[np.complex64]:
     """Given an nxn matrix-like ndarray whose columns constitute a basis for an
     n-dimensional vector space, this function orthonormalizes the basis by the
     Gram-Schmidt procedure.
 
     Args:
-        V (np.ndarray): Matrix-like ndarray whose columns form a basis of a
-                        vector space.
+        V (NDArray[np.complex64]): Matrix-like ndarray whose columns form a 
+                                   basis of a vector space.
 
     Returns:
-        np.ndarray: A unitary matrix-like ndarray that constitues a basis of
-                    the vector space V does.
+        NDArray[np.complex64]: A unitary matrix-like ndarray that constitues a
+                               basis of the vector space V does.
     """
     U = np.zeros_like(V, dtype=V.dtype)
     for idx1 in range(V.shape[-1]):
--- exploration/electroweakino_mixing/code/takagi.py
+++ exploration/electroweakino_mixing/code/takagi.py
@@ -1,77 +1,152 @@
 import numpy as np
-from scipy.linalg import eigh
+from numpy.typing import NDArray
+from scipy.linalg import eigh, eig
 from cmath import phase
 
-from gram_schmidt import complete_nxn_basis, gram_schmidt
+from gram_schmidt import complete_nxn_basis, gram_schmidt, isLinearlyDependent, EPSILON
 from diagonalize import get_finite
 
-from sympy import pprint, Matrix
+TOL = np.sqrt(EPSILON)
 
 
-def find_singval_vector(A: np.ndarray, eigvecs: np.ndarray) -> np.ndarray:
-    """_summary_
+def find_takagi_vector(
+    A: NDArray[np.complex64],
+) -> NDArray[np.complex64]:
+    """For a matrix-like ndarray A, finds a Takagi vector v such that
+    A v* = sigma * v.
 
     Args:
-        A (np.ndarray): _description_
-        eigvecs (np.ndarray): _description_
+        A (NDArray[np.complex64]): Matrix-like ndarray
+
+    Raises:
+        RuntimeError: If Takagi vector could not be found.
 
     Returns:
-        np.ndarray: _description_
+        NDArray[np.complex64]: Takagi vector of A.
     """
-    best_std = np.inf
-    for eigvec in eigvecs:
-        Avbar = A @ eigvec.conj()
-        non_zeros = np.nonzero(get_finite(eigvec))
-        singval = get_finite(np.mean(Avbar[non_zeros] / eigvec[non_zeros]))
-        singval_std = get_finite(abs(np.std(Avbar[non_zeros] / eigvec[non_zeros])))
-        if singval_std < best_std:
-            best_std = singval_std
-            phase_shift = phase(singval) / 2
-            v = np.exp(1j * phase_shift) * eigvec
-    return v
+    # First, the eigenvectors and eigenvalues of AA* are required.
+    eigvals, eigvecs = eigh(A@A.conj())
+    # For there to be a Takagi vector, the eigenvalues must be non-negative.
+    assert any(
+        eigvals >= 0.), "No eigenvalues of AA* are positive - Takagi vector does not exist."
+    # The first non-negative eigenvalue is the smallest such eigenvalues since
+    # eigvals are automatically ordered.
+    first_positive_eigidx = np.where(eigvals >= 0)[0, 0]
+
+    eigval = eigvals[first_positive_eigidx]  # First non-negative eigenvalue
+    x = eigvecs[:, first_positive_eigidx]  # Corresponding eigenvector
+
+    # First assume that Ax* and x are linearly dependent.
+    # Then we would need to check that Ax* = mu * x for some mu.
+    # Avoiding zero-division, we ignore the components where x is zero.
+
+    Ax = A@x.conj()
+    isLinDep, prop = isLinearlyDependent(Ax, x, get_prop=True)
+
+    if isLinDep:
+        v = x
+        mu = prop
+    # If Ax* and x are linearly independent, the vector
+    # y = Ax* + sqrt(eigval)*x should be a Takagi vector.
+    # We do the same with y by testing the linear dependence of Ay* and y.
+    else:
+        print("Trying backup Takagi vector!")
+        y = Ax + np.sqrt(eigval) * x
+        isLinDep, prop = isLinearlyDependent(A@y.conj(), y)
+        if isLinDep:
+            v = y
+            mu = prop
+        else:
+            # If Ay* and y are linearly independent also, then something went
+            # wrong as no Takagi vector was found.
+            raise RuntimeError("Could not find Takagi vector.")
+
+    # If a vector was found, shift the phase such that mu is real and positive.
+    phase_shift = 0.5 * np.angle(mu)
+    return v * np.exp(1j * phase_shift)
 
 
-def triagonalize_matrix(A: np.ndarray) -> np.ndarray:
+def triangularize_matrix(A) -> NDArray[np.complex64]:
     """
     A -> U^dagger A U* = |sigma   * |
                          |  0    A2 |
     where A2 is an (N-1)x(N-1) symmetric matrix.
 
     Args:
-        A (np.ndarray): A symmetric NxN matrix.
+        A (NDArray[np.complex64]): A symmetric NxN matrix.
 
     Returns:
-        np.ndarray: |sigma   * |
-                    |  0    A2 |
-                    where A2 is an (N-1)x(N-1) symmetric matrix.
+        NDArray[np.complex64]: |sigma   * |
+                               |  0    A2 |
+                               where A2 is an (N-1)x(N-1) symmetric matrix.
     """
-    _, eigvecs = eigh(A@A.conj())
-    v = find_singval_vector(A, eigvecs.T)
+    v = find_takagi_vector(A)
     U = complete_nxn_basis(v, n=A.shape[0])
     U = gram_schmidt(U)
     Ubar = U.conj()
     return Ubar.T @ A @ Ubar, Ubar
 
 
-def takagi_factorize(A: np.ndarray):
+def takagi_factorize(A: np.NDArray[np.complex64]):
+    """Diagonalizes symmetric matrix A = U^T D U where
+    U is a unitary matrix and D is a postive, diagonal matrix.
+    Returns D, u
+
+    Args:
+        A (np.NDArray[np.complex64]): Complex nxn matrix-like ndarray.
+
+    Returns:
+        _type_: _description_
+    """
+    assert all(A.T == A), "Matrix is not symmetric - cannot Takagi factorize."
+
     n = A.shape[0]
-    D = np.asarray(A, dtype=complex)
-    V_matrices = np.empty((n, n, n), dtype=complex)
+    D = np.asarray(A.copy(), dtype=complex)
+    U = np.eye(n, dtype=complex)
+
+    # To diagonalize A, we iteratively diagonalize
+    # A -> |sigma  0 |
+    #      | 0    A2 |
+    # one step at a time.
     for idx in range(n):
+        # Current matrix remaining to diagonalize
         Aidx = D[idx:, idx:].copy()
-        D[idx:, idx:], Ubar = triagonalize_matrix(Aidx)
+        # Update relevant part of D and get latest diagonalization matrix Ubar.
+        D[idx:, idx:], Ubar = triangularize_matrix(Aidx)
+        # Embed matrix diagonalizing Aidx into nxn unitary matrix V
         V = np.eye(n, dtype=complex)
         V[idx:, idx:] = Ubar.conj()
-        V_matrices[idx] = V
-    U = np.eye(n, dtype=complex)
-    for V in reversed(V_matrices):
-        U = U @ V.T
-
-    return D, U
+        # Update full diagonalisation matrix.
+        U = V.T @ U
+    # Return abs(D) to ignore any numerical imprecision in positivity.
+    return abs(D), U
 
 
 if __name__ == "__main__":
+    from sympy import pprint, Matrix
     from math import atan, sin, cos, sqrt
+
+    def print_matrix(matrix):
+        pprint(Matrix(get_finite(matrix)))
+        return None
+
+    A = np.array([[0, 1+1j, np.sqrt(2)*1j],
+                  [1+1j, 0, 1+1j],
+                  [np.sqrt(2)*1j, 1+1j, 0]])
+
+    eigvals, _ = eigh(A@A.conj())
+
+    print(eigvals)
+
+    D, N = takagi_factorize(A)
+
+    print_matrix(D)
+    print_matrix(N)
+    print_matrix(N.T@D@N)
+    # print_matrix(abs(N).T@D@abs(N))
+
+    exit()
+
     sinW = sqrt(0.23120)  # sine of the Weinberg mixing angle
     cosW = sqrt(1 - 0.23120)  # cosine of the Weinberg mixing angle
 
@@ -90,16 +165,53 @@ if __name__ == "__main__":
         [-cosbeta * sinW * mZ, cosbeta * cosW * mZ, 0, -mu],
         [sinbeta * sinW * mZ, -sinbeta * cosW * mZ, -mu, 0]
     ])
+    MMdag = M_neutralino @ M_neutralino.conj().T
 
-    triagonalize_matrix()
+    MdagM = M_neutralino.conj().T @ M_neutralino
+
     D, N = takagi_factorize(M_neutralino)
 
-    masses_squared, Nprime = eigh(M_neutralino@M_neutralino.conj().T)
-    Dprime = np.diag(np.sqrt(masses_squared))
-    M_reproduced = N.T@D@N
-    M_reproduced_prime = Nprime.T@Dprime@Nprime
+    print_matrix(N.T @ D @ N - M_neutralino)
 
-    pprint(Matrix(get_finite(M_neutralino - M_reproduced_prime)))
+    '''
+    masses_squared, Nprime = eigh(MMdag)
+    # Nprime = Nprime @ N.conj().T
+    D = np.diag(np.sqrt(masses_squared))
+    N = Nprime.conj().T
+    # Dprime = Nprime.conj() @ M_neutralino @ Nprime.conj().T
+
+    # print_matrix(MMdag - N.conj().T @ D**2 @ N)
+    Mtilde = N.conj().T @ D @ N.conj()
+    # print_matrix(N.T @ D @ N)
+    '''
+    exit()
+
+    # T = (Nprime.T @ M_neutralino @ Nprime).conj()
+
+    # phase_shift = np.diag(np.exp(-0.5j * np.angle(np.diag(T))))
+
+    # Nprime = phase_shift.conj() @ Nprime
+    # Dprime = phase_shift @ T @ phase_shift.T
+
+    # pprint(Matrix(get_finite(phase_shift @ T @ phase_shift.T)))
+    # pprint(Matrix(get_finite(phase_shift.conj().T @ phase_shift)))
+    # pprint(Matrix(get_finite(Nprime.T @ Dprime @ Nprime)))
+    # pprint(Matrix(get_finite(phase_shift)))
+
+    # M_reproduced = N.T@D@N
+    # M_reproduced_prime = Nprime.T@Dprime@Nprime
+
+    # pprint(Matrix(get_finite(M_neutralino)))
+    # pprint(Matrix(get_finite(M_reproduced_prime)))
+    # pprint(Matrix(get_finite(Vprime)))
+    # pprint(Matrix(get_finite(VV.T@Dprime@VV)))
+    # pprint(Matrix(get_finite(Dprime)))
+    # pprint(Matrix(get_finite(D)))
+    exit()
+
+    print(get_finite(np.trace(D@D.conj().T) - np.trace(Dprime@Dprime.conj().T)))
+
+    # pprint(Matrix(get_finite(M_neutralino - M_reproduced_prime)))
     # pprint(Matrix(get_finite(M_neutralino - M_reproduced)))
 
     # pprint(Matrix(get_finite(N.T@M_neutralino@N - Nprime.T@M_neutralino@Nprime)))
