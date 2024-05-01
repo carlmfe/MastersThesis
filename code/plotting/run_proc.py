052280dfc306bd876b03286072c2a14273f665b1
carlmfe <cmfevang@gmail.com>
2024-05-01 14:08:22 +0200

    Update smoking interface to include NLO contribution to higgsino cross-section.

--- code/plotting/run_proc.py
+++ code/plotting/run_proc.py
@@ -12,13 +12,16 @@ SMOKING_ROOT = '/home/carlmfe/Documents/master/smoking/'
 available_pdf_sets = [ f.name for f in os.scandir(LHAPDF_ROOT) if f.is_dir() ]
 
 neu_pids = [1000022, 1000023, 1000025, 1000035]
+slep_pids = [1000011, 1000013, 1000015, 2000011, 2000013, 2000015, -1000011, -1000013, -1000015, -2000011, -2000013, -2000015]
+sneu_pids = [1000012, 1000014, 1000016]
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--code', '-c', default='smoking', choices=['smoking', 'resummino'])
-parser.add_argument('--pid1', dest='pid1', default=1000022, choices=neu_pids, type=int)
-parser.add_argument('--pid2', dest='pid2', default=1000023, choices=neu_pids, type=int)
+parser.add_argument('--pid1', dest='pid1', default=1000022, choices=neu_pids+slep_pids+sneu_pids, type=int)
+parser.add_argument('--pid2', dest='pid2', default=1000023, choices=neu_pids+slep_pids+sneu_pids, type=int)
 parser.add_argument('-r', '--readfile', dest='readfile', default=SMOKING_ROOT+'example/sps1a.slha')
 parser.add_argument('-P', '--PDF', dest='pdf_set', default='PDF4LHC21_40_pdfas', choices=available_pdf_sets)
+parser.add_argument('--nlo', dest='NLO', action='store_true')
 parser.add_argument('--tnum', dest='tnum', action='store_true')
 parser.add_argument('-S', '--center-of-mass-energy', dest='S', default=13600, type=float)
 parser.add_argument('-v', '--verbose', dest='verbose', action='store_true')
@@ -26,6 +29,7 @@ parser.add_argument('-o', '--outifle', dest='outfile', default='__temp')
 parser.add_argument('-l', '--label', dest='label', default=None)
 parser.add_argument('--tempstem', dest='tempstem', default=None)
 clargs = parser.parse_args()
+
 
 pid1 = int(clargs.pid1)
 pid2 = int(clargs.pid2)
@@ -42,11 +46,11 @@ else:
 
 tmpresfile = mktmpfile(extension = '.out', stem = clargs.tempstem)
 if clargs.code == 'smoking':
-    run_cross_section(clargs.readfile, tmpresfile, pid1, pid2, clargs.tnum)
+    run_cross_section(clargs.readfile, tmpresfile, pid1, pid2, clargs.tnum, clargs.NLO)
 elif clargs.code == 'resummino':
-    run_resummino(clargs.readfile, (pid1, pid2), tmpresfile, clargs.tempstem)
+    run_resummino(clargs.readfile, (pid1, pid2), tmpresfile, clargs.tempstem, clargs.NLO)
 
-write_results(tmpresfile, outfile, (pid1, pid2), clargs.label)
+write_results(tmpresfile, outfile, (pid1, pid2), clargs.label, order = 1 if clargs.NLO else 0)
 os.remove(tmpresfile)
 
 set_pdf_set(original_pdf_set)
