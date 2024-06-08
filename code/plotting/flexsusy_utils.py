010dd7266e253d9bba2996c62f8e82803fb14498
carlmfe <cmfevang@gmail.com>
2024-06-08 12:56:09 +0200

    Generate all .slha files used to make figure.

--- code/plotting/flexsusy_utils.py
+++ code/plotting/flexsusy_utils.py
@@ -190,41 +190,175 @@ imusqmix_block = '''Block IMUSQMIX
 
 if __name__ == '__main__':
 
-    from math import cos, sin, pi
+    from math import cos, sin, pi, exp, log
     import pyslha
+    from slha_utils import copy_slha_with_replacement
 
-    # test_root = 'slha//muCPV/'
-    test_root = 'slha/hinoCPV/'
 
-    # slha_filename = 'csps1a.in.CMSSMCPV'
-    slha_filename = 'hino.in.MSSMCPV'
+    #######################################################
+    ### Generate slha files for squark/gluino mass plot ###
+    #######################################################
+    '''
+    '''
+    test_root = 'slha/squarkGluinoMass/'
+
+    # slha_dir = 'csps1a_unnatural/'
+    # slha_dir = 'hino_unnatural/'
+    slha_dir = 'hino_unnatural_loggrid/'
+
+
+    FLEXIBLESUSY_MODEL = 'MSSMCPV'
+
+    # slha_filename = 'csps1a_unnatural.in'
+    slha_filename = 'hino_unnatural.in'
+    # slha_filestem = 'csps1a'
+    slha_filestem = 'hino'
+
+    n = 10
+
+    param_points = []
+    M0 = 200
+    Mf = 1100
+
+    # for idx1 in range(n):
+    #     usq_mass = (Mf-M0)/(n-1) * idx1 + M0
+    #     for idx2 in range(n):
+    #         dsq_mass = (Mf-M0)/(n-1) * idx2 + M0
+
+    #         param_points.append({'@u' : str(usq_mass), '@d' : str(dsq_mass)})
+
+    # for idx1 in range(n):
+    #     squark_mass = (Mf-M0)/(n-1) * idx1 + M0
+    #     for idx2 in range(n):
+    #         gluino_mass = (Mf-M0)/(n-1) * idx2 + M0
+
+    #         param_points.append({'@g' : str(gluino_mass), '@m' : str(squark_mass)})
+
+    for idx1 in range(2*n):
+        squark_mass = M0**(1 - idx1/(2*n-1)) * Mf**(idx1/(2*n-1))
+        for idx2 in range(2*n):
+            gluino_mass = M0**(1 - idx2/(2*n-1)) * Mf**(idx2/(2*n-1))
+
+            param_points.append({'@g' : str(gluino_mass), '@m' : str(squark_mass)})
+
+    with open(test_root + slha_dir + slha_filestem + '.info', 'w') as infofile:
+        for idx, param_rules in enumerate(param_points):
+            replace_rules = [[key, val] for key, val in param_rules.items()]
+            copy_slha_with_replacement(test_root + slha_filename, test_root + slha_dir + slha_filestem + '_' + str(idx) + '.slha', replace_rules)
+            infofile.write(f"{slha_filestem + '_' + str(idx) + '.slha'} : {list(param_rules.values())}\n")
+
+
+
+
+
+    exit(1)
+
+    #######################################
+    ### Generate slha files for mu plot ###
+    #######################################
+    '''
+    test_root = 'slha/muCPV/'
+    # test_root = 'slha/hinoCPV/'
+    FLEXIBLESUSY_MODEL = 'CMSSMCPV'
+    # FLEXIBLESUSY_MODEL = 'MSSMCPV'
+
+    slha_filename = 'csps1a.in.CMSSMCPV'
+    # slha_filename = 'hino.in.MSSMCPV'
 
     n = 50
 
     # param_points = [{'@c' : str(cos((2*i-n)/n*pi)), '@s' : str(sin((2*i-n)/n*pi)), '@p' : str((2*i-n)/n*pi)} for i in range(n)]
-    absMu = 100
-    param_points = [{'@r' : str(absMu*cos((2*i-n)/n*pi)), '@i' : str(absMu*sin((2*i-n)/n*pi))} for i in range(n)]
+    param_points = [{'@c' : str(cos((2*i-n)/n*pi)), '@s' : str(sin((2*i-n)/n*pi))} for i in range(n)]
+    # absMu = 100
+    # param_points = [{'@r' : str(absMu*cos((2*i-n)/n*pi)), '@i' : str(absMu*sin((2*i-n)/n*pi))} for i in range(n)]
+    '''
 
-    # generate_infiles(test_root+slha_filename, param_points, test_root+'input', extension='.in.CMSSMCPV')
+
+    #########################################
+    ### Generate slha files for colorplot ###
+    #########################################
+    '''
+    test_root = 'slha/hinoMassCPV/'
+    FLEXIBLESUSY_MODEL = 'MSSMCPV'
+
+    slha_filename = 'hino.in.MSSMCPV'
+
+    n = 10
+
+    param_points = []
+    mu0 = 100
+    muf = 1000
+    for idx1 in range(n):
+        absMu = (muf-mu0)/(n-1) * idx1 + mu0
+        for idx2 in range(n):
+            cphi = cos((2*idx2-n)/n*pi)
+            sphi = sin((2*idx2-n)/n*pi)
+            param_points.append({'@r' : str(absMu * cphi), '@i' : str(absMu * sphi)})
+    '''
+
+
+    ################################################
+    ### Generate slha files for squark mass plot ###
+    ################################################
+
+    '''
+    test_root = 'slha/squarkGluinoMass/'
+    FLEXIBLESUSY_MODEL = 'MSSMCPV'
+
+    slha_filename = 'hino.in.MSSMCPV'
+
+    n = 10
+
+    param_points = []
+    M0 = 1000
+    Mf = 2000
+    for idx1 in range(n):
+        squark_mass = (Mf-M0)/(n-1) * idx1 + M0
+        for idx2 in range(n):
+            gluino_mass = (Mf-M0)/(n-1) * idx2 + M0
+
+            param_points.append({'@g' : str(gluino_mass), '@m' : str(squark_mass)})
+    '''
+
+    ############################################
+    ### Generate slha files for abs(mu) plot ###
+    ############################################
+    '''
+    test_root = 'slha/hinoError/'
+    FLEXIBLESUSY_MODEL = 'MSSMCPV'
+    
+    slha_filename = 'hino.in.MSSMCPV'
+
+    n = 30
+
+    mumin = 100
+    mumax = 1000
+
+    param_points = [{'@m' : str((mumax - mumin)*i/(n-1) + mumin)} for i in range(n)]
+    '''
+
+    ########################
+    ### Generate spectra ###
+    ########################
 
     if not os.path.exists(test_root+'input'):
         os.mkdir(test_root+'input')
     if not os.path.exists(test_root+'generated'):
         os.mkdir(test_root+'generated')
 
-    generate_infiles(test_root+slha_filename, param_points, test_root+'input', extension='.in.MSSMCPV')
+    generate_infiles(test_root+slha_filename, param_points, test_root+'input', extension=f'.in.{FLEXIBLESUSY_MODEL}')
 
     infiles = [ test_root+'input/'+f.name for f in os.scandir(test_root+'input') if f.is_file() ]
 
-    # generate_spectra('CMSSMCPV', infiles, test_root+'generated')
-    generate_spectra('MSSMCPV', infiles, test_root+'generated')
+    generate_spectra(FLEXIBLESUSY_MODEL, infiles, test_root+'generated')
 
-    for spectrumfile in [ test_root+'generated/'+f.name for f in os.scandir(test_root+'generated/') if f.is_file()]:
+    for spectrumfile in [ test_root+'generated/'+f.name for f in os.scandir(test_root+'generated/') if f.is_file() and f.name[-4:] != 'info']:
         
         readfile = pyslha.read(spectrumfile, ignorenomass=True)
 
         try:
             readfile.blocks['IMUSQMIX']
         except KeyError:
+            print(f'Padding imaginary blocks  in {spectrumfile}!')
             pad_flexiblesusy_spectrum(spectrumfile)
 
